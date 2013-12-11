0a1ae81edcb020d226e66309af13809c40c802fc
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-11 12:13:27 +0200

    clean-up tests

--- source/client.js
+++ source/client.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var query = require('./query');
+var querystring = require('querystring');
 
 module.exports = function (app, options) {
 	if (!app) {
@@ -44,6 +44,20 @@ module.exports = function (app, options) {
 
 		callback(null, accessToken);
 	}
+
+	var query = {
+		events: function (params) {
+			if (typeof params === 'string') {
+				return '?event=' + params;
+			}
+
+			return '?' + querystring.stringify(params);
+		},
+
+		reports: function (params) {
+			return '?' + querystring.stringify(params);
+		}
+	};
 
 	var client = function client(event, data, callback) {
 		var url = server + '/api/events/' + app;
--- test/utils.js
+++
@@ -1,7 +0,0 @@
-function getRootUrl () {
-	return 'http://localhost:3005/';
-}
-
-module.exports = {
-	getRootUrl: getRootUrl
-};
